300f0a82844dd80485b0a54f8b88f96e254ff45e
juju2181 <anishshilpakar8@gmail.com>
2022-11-30 16:51:55 +0545

    Added better solution for the isomorphic strings

--- leetcode_isomorphic_strings.py
+++ leetcode_isomorphic_strings.py
@@ -1,55 +1,19 @@
-"""
-Given two strings s and t, determine if they are isomorphic.
-
-Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-
-All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
-
- 
-
-Example 1:
-
-Input: s = "egg", t = "add"
-Output: true
-Example 2:
-
-Input: s = "foo", t = "bar"
-Output: false
-Example 3:
-
-Input: s = "paper", t = "title"
-Output: true
- 
-
-Constraints:
-
-1 <= s.length <= 5 * 104
-t.length == s.length
-s and t consist of any valid ascii character.
-"""
-
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        # Using hashmap for solution
         #return false if the length differs 
         if len(s) != len(t):
             return False
-        # initializing a dictionary to store letters from s and t as key value pairs
-        charCount = {} 
-        #iterating over s and t 
-        for i in range(len(s)):
-            #if s[i] is a key in charCount
-            if s[i] in charCount:
-                #get corresponding value for the key
-                c = charCount[s[i]]
-                #check with another string and return corresponding result
-                if c != t[i]:
-                    return False 
-            #if t[i] is not a value in charCount 
-            elif t[i] not in charCount.values():
-                charCount[s[i]] = t[i]
-            else:
-                return False
+        #initializing hashmaps
+        mapST, mapTS = {},{} 
+        #Loop through each character of both strings 
+        for c1,c2 in zip(s,t):
+            #check if the character exists in the hashmap and the mapping to character of next string also exist
+            if ((c1 in mapST and mapST[c1] != c2) or (c2 in mapTS and mapTS[c2] != c1)):
+                return False 
+            #perform mapping from one string to another 
+            mapST[c1] = c2 
+            mapTS[c2] = c1
+        #if mapping is success for each character return True as it is isomorphic
         return True
     
 if __name__ == "__main__":
